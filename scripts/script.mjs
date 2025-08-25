63958d9e4a22c64175d06aa71a56aa2ceb1ff1b5
Liam <77292172+lreboseno22@users.noreply.github.com>
2025-08-25 12:39:46 -0400

    get list of pokemon and random id number

--- scripts/script.mjs
+++ scripts/script.mjs
@@ -1,6 +1,12 @@
 document.getElementById("fetchPokemon").addEventListener('click', async () =>{
     try {
+        // get random pokemon from list of pokemon
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1');
+        const data = await response.json();
         
+        const totalPokmeon = data.count;
+        const randomId = Math.floor(Math.random() * totalPokmeon) + 1; // random number to associate with pokemon id 1 - totalPokemonCount
+        console.log(randomId);
     } catch (err){
         alert(err.message);
     }

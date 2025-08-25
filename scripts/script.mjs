2ed1fa058d8580dc9405702e5f9a259b0baa9089
Liam <77292172+lreboseno22@users.noreply.github.com>
2025-08-25 12:47:37 -0400

    get pokemon data based on randomId

--- scripts/script.mjs
+++ scripts/script.mjs
@@ -3,10 +3,15 @@ document.getElementById("fetchPokemon").addEventListener('click', async () =>{
         // get random pokemon from list of pokemon
         const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1');
         const data = await response.json();
-        
-        const totalPokmeon = data.count;
-        const randomId = Math.floor(Math.random() * totalPokmeon) + 1; // random number to associate with pokemon id 1 - totalPokemonCount
-        console.log(randomId);
+        const totalPokemeon = data.count;
+
+        const randomId = Math.floor(Math.random() * totalPokemeon) + 1; // random number to associate with pokemon id 1 - totalPokemonCount
+        console.log("Random pokemon id", randomId);
+
+        // get pokemon info based on randomId
+        const pokemonResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`);
+        const pokemonData = await pokemonResponse.json();
+        console.log("Pokemon data:", pokemonData);
     } catch (err){
         alert(err.message);
     }
